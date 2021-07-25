0164e7b3552069f25903204dc9fbc99615e95365
ARTushar <1605070@ugrad.cse.buet.ac.bd>
2021-07-25 15:07:19 +0600

    handle empty fields

--- pages/api/tutors/search.ts
+++ pages/api/tutors/search.ts
@@ -1,11 +1,19 @@
 import {createNC} from "../../../lib/utils/ncHandler";
 import ShortTutor from "../../../lib/models/tutor/shortTutor";
+import {debug, getKeys} from "../../../lib/utils/helpers";
 
 const handler = createNC();
 
 handler
     .get(async (req, res, next) => {
+        debug("filter", "query", req.query);
         try {
+            for(const key of getKeys(req.query)) {
+                if(req.query[key] && req.query[key].length == 0) {
+                    delete req.query[key];
+                }
+            }
+            debug("after query filter", "query", req.query);
             const results = await ShortTutor.getTutors(req.query);
             res.status(200).json(results);
         } catch (e) {
